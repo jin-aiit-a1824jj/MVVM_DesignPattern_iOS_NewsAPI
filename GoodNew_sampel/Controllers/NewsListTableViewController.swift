2d29d44e35c1efeb8acc7a05f83828e6c7b540c2
a1824jj <a1824jj@aiit.ac.jp>
2020-03-30 15:52:10 +0900

    make project & ui

---
+++ GoodNew_sampel/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NewsListTableViewController.swift
+//  GoodNew_sampel
+//
+//  Created by JONGWOO JIN on 2020/03/30.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewsListTableViewController: UITableViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup(){
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+    
+}
