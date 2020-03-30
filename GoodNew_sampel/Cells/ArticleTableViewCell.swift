ce7154d000955943bd436614b3fbd92fbd1f0f00
a1824jj <a1824jj@aiit.ac.jp>
2020-03-30 16:32:03 +0900

    add custom cell

---
+++ GoodNew_sampel/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNew_sampel
+//
+//  Created by JONGWOO JIN on 2020/03/30.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
